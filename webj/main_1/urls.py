6abba95de7e79ddcca42e175e6b867716ad763dd
Pluxxa <95534978+Pluxxa@users.noreply.github.com>
2024-07-15 14:22:00 +0300

    add code

--- webj/main_1/urls.py
+++ webj/main_1/urls.py
@@ -1,7 +1,7 @@
+# main_1/urls.py
 from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.data, name='data'),     # Главная страница для main_1
-    path('test/', views.test, name='test')  # Страница test для main_1
+    path('new/', views.new, name='new'),
 ]
--- webj/main_1/views.py
+++ webj/main_1/views.py
@@ -1,7 +1,19 @@
-from django.shortcuts import render
+# main_1/views.py
 
-def data(request):
-    return render(request, 'main_1/data.html')
+from django.shortcuts import render, redirect
+from .models import Message
 
-def test(request):
-    return render(request, 'main_1/test.html')
+def new(request):
+    if request.method == 'POST':
+        text = request.POST.get('text')
+        x_position = request.POST.get('x_position')
+        y_position = request.POST.get('y_position')
+        if text and x_position and y_position:
+            message = Message(text=text, x_position=x_position, y_position=y_position)
+            message.save()
+            return redirect('index')
+    return render(request, 'new.html')
+
+def index(request):
+    messages = Message.objects.all()
+    return render(request, 'index.html', {'messages': messages})
